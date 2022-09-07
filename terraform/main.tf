7e13569bc0cdb49e22c1537f166411c601a2f9b4
Joe Ingledew <joe.ingledew@ofqual.gov.uk>
2022-09-07 12:34:22 +0100

    Correct TF file again - remove reserved=true

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,6 @@ resource "azurerm_service_plan" "asp" {
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     os_type = "Linux"
-    reserved = true
     sku_name = "B1"
 
     tags = {
